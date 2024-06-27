62664443f6d7528231c2b17695740687252304af
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-06-27 13:09:36 +0300

    commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,16 @@
 // Add imports above this line
+import simpleLightbox from 'simplelightbox';
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
-console.log(`hello`);
+console.log(simpleLightbox);
+
+const galleryEl = document.querySelector('.gallery');
+const markup = onCreateMarkup(galleryItems);
+
+function onCreateMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    console.log(preview);
+  });
+}
