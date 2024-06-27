4fcebba14b7e179c7296252b0aa14adb704d7574
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-06-27 13:16:28 +0300

    1111

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -8,9 +8,22 @@ console.log(simpleLightbox);
 
 const galleryEl = document.querySelector('.gallery');
 const markup = onCreateMarkup(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', markup);
 
 function onCreateMarkup(galleryItems) {
-  return galleryItems.map(({ preview, original, description }) => {
-    console.log(preview);
-  });
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+                <a class="gallery__link" href="large-image.jpg">
+                    <img
+                        class=" "
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+            
+                    />
+                </a>
+            </li>`;
+    })
+    .join('');
 }
