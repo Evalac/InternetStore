18affaf6e8626997862122e05286d00c0fb6b9c9
Evalac <maslenokprograming@gmail.com>
2024-07-02 13:31:37 +0300

    11111

--- src/js/05-internetStore.js
+++ src/js/05-internetStore.js
@@ -1,3 +1,28 @@
+{
+  // setItem - add value
+  // getItem - get value
+  // removeItem - delete item
+  // clear - delete all
+  localStorage.setItem('TEST_KEY', 'Hello world');
+  localStorage.setItem('Array', JSON.stringify([1, 2, 3, 4, 5])); // тільки таким чином можна додати масив в локальне сховище щоб його потім можна було витягнути  JSON.stringify()
+  localStorage.setItem('Object', JSON.stringify({ name: 'mango' }));
+  // localStorage.removeItem('TEST_KEY');
+
+  console.log(localStorage.getItem('TEST_KEY'));
+  console.log(JSON.parse(localStorage.getItem('Array')));
+  console.log(JSON.parse(localStorage.getItem('Object')));
+
+  const localBtn = document.querySelector('.js-local');
+  const sessionBtn = document.querySelector('.js-session');
+  localBtn.addEventListener('click', () => {
+    localStorage.setItem('local', 'Storage');
+  });
+
+  sessionBtn.addEventListener('click', () => {
+    sessionStorage.setItem('session', 'Storage');
+  });
+}
+
 const instruments = [
   {
     id: 1,
@@ -78,26 +103,3 @@ const instruments = [
       'Електрична шліфувальна машина для обробки деревини та металу.',
   },
 ];
-
-// setItem - add value
-// getItem - get value
-// removeItem - delete item
-// clear - delete all
-localStorage.setItem('TEST_KEY', 'Hello world');
-localStorage.setItem('Array', JSON.stringify([1, 2, 3, 4, 5])); // тільки таким чином можна додати масив в локальне сховище щоб його потім можна було витягнути  JSON.stringify()
-localStorage.setItem('Object', JSON.stringify({ name: 'mango' }));
-localStorage.removeItem('TEST_KEY');
-
-console.log(localStorage.getItem('TEST_KEY'));
-console.log(JSON.parse(localStorage.getItem('Array')));
-console.log(JSON.parse(localStorage.getItem('Object')));
-
-const localBtn = document.querySelector('.js-local');
-const sessionBtn = document.querySelector('.js-session');
-localBtn.addEventListener('click', () => {
-  localStorage.setItem('local', 'Storage');
-});
-
-sessionBtn.addEventListener('click', () => {
-  sessionStorage.setItem('session', 'Storage');
-});
