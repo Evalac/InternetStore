fe8cf9ab4824f09fe491c13dad8bbddf24cdc711
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-03 13:36:30 +0300

    1111

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-import * as basicLightbox from "basiclightbox";
-import "src/style/style.css";
+import * as basicLightbox from 'basiclightbox';
+import 'src/styles/main';
 console.log(basicLightbox);
 // {
 //   // setItem - add value
@@ -29,81 +29,81 @@ console.log(basicLightbox);
 const instruments = [
   {
     id: 1,
-    img: "https://static.dnipro-m.ua/cache/products/1754/catalog_origin_316727.jpg",
-    name: "Молоток",
+    img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_316727.jpg',
+    name: 'Молоток',
     price: 10.99,
     description:
       "Міцний сталевий молоток з дерев'яною ручкою, підходить для загального використання.",
   },
   {
     id: 2,
-    img: "https://static.dnipro-m.ua/cache/products/2801/catalog_origin_323389.jpg",
-    name: "Викрутка",
+    img: 'https://static.dnipro-m.ua/cache/products/2801/catalog_origin_323389.jpg',
+    name: 'Викрутка',
     price: 5.49,
     description:
-      "Викрутка з хрестоподібним наконечником, ідеальна для загвинчування шурупів.",
+      'Викрутка з хрестоподібним наконечником, ідеальна для загвинчування шурупів.',
   },
   {
     id: 3,
-    img: "https://static.dnipro-m.ua/cache/products/4925/catalog_origin_326985.jpg",
-    name: "Ключ",
+    img: 'https://static.dnipro-m.ua/cache/products/4925/catalog_origin_326985.jpg',
+    name: 'Ключ',
     price: 7.89,
     description:
-      "Регульований ключ для затягування та відкручування гайок різних розмірів.",
+      'Регульований ключ для затягування та відкручування гайок різних розмірів.',
   },
   {
     id: 4,
-    img: "https://static.dnipro-m.ua/cache/products/2782/catalog_origin_316783.jpg",
-    name: "Плоскогубці",
+    img: 'https://static.dnipro-m.ua/cache/products/2782/catalog_origin_316783.jpg',
+    name: 'Плоскогубці',
     price: 8.25,
     description:
-      "Універсальні плоскогубці з гумовими ручками для зручного захвату.",
+      'Універсальні плоскогубці з гумовими ручками для зручного захвату.',
   },
   {
     id: 5,
-    img: "https://static.dnipro-m.ua/cache/products/2996/catalog_origin_317283.jpg",
-    name: "Пила",
+    img: 'https://static.dnipro-m.ua/cache/products/2996/catalog_origin_317283.jpg',
+    name: 'Пила',
     price: 12.3,
     description:
-      "Ручна пила з загартованими зубцями для легкого різання деревини.",
+      'Ручна пила з загартованими зубцями для легкого різання деревини.',
   },
   {
     id: 6,
-    img: "https://static.dnipro-m.ua/cache/products/2193/catalog_origin_326103.jpg",
-    name: "Рулетка",
+    img: 'https://static.dnipro-m.ua/cache/products/2193/catalog_origin_326103.jpg',
+    name: 'Рулетка',
     price: 4.99,
     description:
       "Рулетка з м'яким корпусом та висувною стрічкою довжиною 5 метрів.",
   },
   {
     id: 7,
-    img: "https://static.dnipro-m.ua/cache/products/2022/catalog_origin_323412.jpg",
-    name: "Рівень",
+    img: 'https://static.dnipro-m.ua/cache/products/2022/catalog_origin_323412.jpg',
+    name: 'Рівень',
     price: 6.75,
     description:
-      "Будівельний рівень для точної перевірки горизонтальних та вертикальних поверхонь.",
+      'Будівельний рівень для точної перевірки горизонтальних та вертикальних поверхонь.',
   },
   {
     id: 8,
-    img: "https://static.dnipro-m.ua/cache/products/5883/catalog_origin_320255.jpg",
-    name: "Канцелярський ніж",
+    img: 'https://static.dnipro-m.ua/cache/products/5883/catalog_origin_320255.jpg',
+    name: 'Канцелярський ніж',
     price: 3.49,
-    description: "Ніж з висувним лезом для точного різання паперу та картону.",
+    description: 'Ніж з висувним лезом для точного різання паперу та картону.',
   },
   {
     id: 9,
-    img: "https://static.dnipro-m.ua/cache/products/1219/catalog_origin_325151.jpg",
-    name: "Дриль",
+    img: 'https://static.dnipro-m.ua/cache/products/1219/catalog_origin_325151.jpg',
+    name: 'Дриль',
     price: 49.99,
-    description: "Електричний дриль з набором свердел для різних матеріалів.",
+    description: 'Електричний дриль з набором свердел для різних матеріалів.',
   },
   {
     id: 10,
-    img: "https://static.dnipro-m.ua/cache/products/1452/catalog_origin_317290.jpg",
-    name: "Шліфувальна машина",
+    img: 'https://static.dnipro-m.ua/cache/products/1452/catalog_origin_317290.jpg',
+    name: 'Шліфувальна машина',
     price: 35.6,
     description:
-      "Електрична шліфувальна машина для обробки деревини та металу.",
+      'Електрична шліфувальна машина для обробки деревини та металу.',
   },
 ];
 
@@ -116,10 +116,10 @@ const instruments = [
 //     "Міцний сталевий молоток з дерев'яною ручкою, підходить для загального використання.",
 // },
 
-const search = document.querySelector(".js-search");
+const search = document.querySelector('.js-search');
 const markupInstrument = createMarkup(instruments);
 
-const cartEl = document.querySelector(".js-list");
+const cartEl = document.querySelector('.js-list');
 cartEl.innerHTML = markupInstrument;
 
 function createMarkup(instruments) {
@@ -135,14 +135,14 @@ function createMarkup(instruments) {
         </div>
       </li>`;
     })
-    .join("");
+    .join('');
 }
-cartEl.addEventListener("click", onclick);
+cartEl.addEventListener('click', onclick);
 
 function onclick(evt) {
   evt.preventDefault();
-  if (evt.target.classList.contains("js-info")) {
-    const { id } = evt.target.closest(".cart--Iteam").dataset; // візьми з нашого елемента по якому ми клікнули першого батька з классом '.cart--Iteam' і з його властивусті дай дата атребут
+  if (evt.target.classList.contains('js-info')) {
+    const { id } = evt.target.closest('.cart--Iteam').dataset; // візьми з нашого елемента по якому ми клікнули першого батька з классом '.cart--Iteam' і з його властивусті дай дата атребут
     const product = findProduct(Number(id));
     console.log(product);
 
