7cd285875761d94204bf7688ad7b61031f76591e
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-03 13:33:10 +0300

    111

--- jstestImport/validate-password.js
+++
@@ -1,7 +0,0 @@
-export const fetchAllUsers = () => {
-  console.log('fetchAllUsers: ', fetchAllUsers);
-};
-
-export const fetchUsersByID = id => {
-  console.log('fetchUsersByID: ', fetchUsersByID);
-};
