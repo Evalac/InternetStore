a6b68bb63003d71ab84c048b658aaa57d2f04eaa
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-04 19:03:52 +0300

    1111

--- src/js/favorite.js
+++ src/js/favorite.js
@@ -7,14 +7,4 @@ const favorite = JSON.parse(localStorage.getItem(common.KEY_FAVORITE)) ?? [];
 const markup = createMarkup(favorite);
 listEl.innerHTML = markup;
 
-function onclick(evt) {
-  evt.preventDefault();
-  if (evt.target.classList.contains('js-info')) {
-    const id = evt.target.closest('.cart--Iteam').dataset.id; // візьми з нашого елемента по якому ми клікнули першого батька з классом '.cart--Iteam' і з його властивусті дай дата атребут
-    const product = findProduct(Number(id));
-
-    onOpenModal(product);
-  }
-}
-
 // додати якийсь хрестик щоб видалити з обраного
