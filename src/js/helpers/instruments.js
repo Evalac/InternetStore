66e5e2691de911d21dd1e45ee478d91a85f0c0b4
Evalac <maslenokprograming@gmail.com>
2024-07-04 18:25:33 +0300

    1111

---
+++ src/js/helpers/instruments.js
@@ -0,0 +1,81 @@
+const instruments = [
+  {
+    id: 1,
+    img: 'https://static.dnipro-m.ua/cache/products/1754/catalog_origin_316727.jpg',
+    name: 'Молоток',
+    price: 10.99,
+    description:
+      "Міцний сталевий молоток з дерев'яною ручкою, підходить для загального використання.",
+  },
+  {
+    id: 2,
+    img: 'https://static.dnipro-m.ua/cache/products/2801/catalog_origin_323389.jpg',
+    name: 'Викрутка',
+    price: 5.49,
+    description:
+      'Викрутка з хрестоподібним наконечником, ідеальна для загвинчування шурупів.',
+  },
+  {
+    id: 3,
+    img: 'https://static.dnipro-m.ua/cache/products/4925/catalog_origin_326985.jpg',
+    name: 'Ключ',
+    price: 7.89,
+    description:
+      'Регульований ключ для затягування та відкручування гайок різних розмірів.',
+  },
+  {
+    id: 4,
+    img: 'https://static.dnipro-m.ua/cache/products/2782/catalog_origin_316783.jpg',
+    name: 'Плоскогубці',
+    price: 8.25,
+    description:
+      'Універсальні плоскогубці з гумовими ручками для зручного захвату.',
+  },
+  {
+    id: 5,
+    img: 'https://static.dnipro-m.ua/cache/products/2996/catalog_origin_317283.jpg',
+    name: 'Пила',
+    price: 12.3,
+    description:
+      'Ручна пила з загартованими зубцями для легкого різання деревини.',
+  },
+  {
+    id: 6,
+    img: 'https://static.dnipro-m.ua/cache/products/2193/catalog_origin_326103.jpg',
+    name: 'Рулетка',
+    price: 4.99,
+    description:
+      "Рулетка з м'яким корпусом та висувною стрічкою довжиною 5 метрів.",
+  },
+  {
+    id: 7,
+    img: 'https://static.dnipro-m.ua/cache/products/2022/catalog_origin_323412.jpg',
+    name: 'Рівень',
+    price: 6.75,
+    description:
+      'Будівельний рівень для точної перевірки горизонтальних та вертикальних поверхонь.',
+  },
+  {
+    id: 8,
+    img: 'https://static.dnipro-m.ua/cache/products/5883/catalog_origin_320255.jpg',
+    name: 'Канцелярський ніж',
+    price: 3.49,
+    description: 'Ніж з висувним лезом для точного різання паперу та картону.',
+  },
+  {
+    id: 9,
+    img: 'https://static.dnipro-m.ua/cache/products/1219/catalog_origin_325151.jpg',
+    name: 'Дриль',
+    price: 49.99,
+    description: 'Електричний дриль з набором свердел для різних матеріалів.',
+  },
+  {
+    id: 10,
+    img: 'https://static.dnipro-m.ua/cache/products/1452/catalog_origin_317290.jpg',
+    name: 'Шліфувальна машина',
+    price: 35.6,
+    description:
+      'Електрична шліфувальна машина для обробки деревини та металу.',
+  },
+];
+export { instruments };
