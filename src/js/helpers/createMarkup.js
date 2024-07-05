90918059986bcaed14f49aef4eb5f62ad5831ee5
Maslenok.M <102866941+Evalac@users.noreply.github.com>
2024-07-05 17:06:35 +0300

    1111

--- src/js/helpers/createMarkup.js
+++ src/js/helpers/createMarkup.js
@@ -1,8 +1,10 @@
 function createMarkup(instruments, cartEl) {
-  const markup = instruments
-    .map(
-      ({ id, img, name, price, description }) =>
-        `<li class="cart--Iteam" data-id="${id}">
+  let markup;
+  if (instruments.length) {
+    markup = instruments
+      .map(
+        ({ id, img, name, price, description }) =>
+          `<li class="cart--Iteam" data-id="${id}">
         <img src="${img}" alt="${name}" width="300px" />
         <h1 class="item--Name">${name}</h1>
         <p ><a class="js-info" href="#">More information</a></p>
@@ -11,8 +13,18 @@ function createMarkup(instruments, cartEl) {
         <button class="js-basket">Add to basket</button>
         </div>
       </li>`
-    )
-    .join('');
+      )
+      .join('');
+  } else {
+    markup = ` <li>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSd-_lSWMDc3yPDlT6t8I4A0XzpY-g26Ta9Gw&s"
+        alt="empty"
+        width="300px"
+      />
+    </li> `;
+  }
+
   cartEl.innerHTML = markup;
 }
 
